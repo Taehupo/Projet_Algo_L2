92bccafa415a92b589098bbcd4dc72f27b2fde26
Taehupo <uzumakigip@gmail.com>
2017-01-26 10:42:59 +0100

    Ajout de test, villes.c villes.h et du code des fichier

---
+++ villes.h
@@ -0,0 +1,16 @@
+#ifndef VILLES_H
+#define VILLES_H
+
+typedef unsigned int uint;
+
+typedef struct
+{
+	uint abscisse;
+	uint ordonnee;
+}ville;
+
+void init_ville(ville *, uint, uint);
+uint get_abs(ville *);
+uint get_ord(ville *);
+
+#endif
