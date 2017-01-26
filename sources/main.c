94d537b7f93f4407df77d5a1e3a1179d42694eb4
Taehupo <uzumakigip@gmail.com>
2017-01-26 15:00:14 +0100

    Début des commentaires

--- sources/main.c
+++ sources/main.c
@@ -21,27 +21,27 @@ int main(int argc, char const *argv[])
 
 	fichier = fopen("test", "r");
 
-	if (fichier != NULL)
+	if (fichier != NULL) 													// Si le fichier existe
 	{
-		fgets(parsing, 100, fichier);
-		ville_table_size = atoi(parsing);
-		ville_table = (ville *) malloc(sizeof(ville) * ville_table_size);
-		printf("Il y a %d villes\n", ville_table_size);
+		fgets(parsing, 100, fichier);										// Récupérer la première ligne
+		ville_table_size = atoi(parsing);									// Récuprere le nombre de ville
+		ville_table = (ville *) malloc(sizeof(ville) * ville_table_size);	// Allocation du tableau de villes
+		printf("Il y a %d villes\n", ville_table_size);						//Debug
 
 		for (int i = 0; i < ville_table_size; ++i)
 		{
-			fgets(parsing, 100, fichier);
+			fgets(parsing, 100, fichier);									// récup des donnée de la ville
 			int tmp1;
 			int tmp2;
 			int j;
-			tmp1 = atoi(parsing);
+			tmp1 = atoi(parsing);											// récup le premier nombre
 			int tmp = tmp1;
-			for(j = 0; tmp != 0 ; ++j)
+			for(j = 0; tmp != 0 ; ++j)										// calcul de la place du second chiffre
 			{
 				tmp = tmp / 10;
 			}
-			tmp2 = atoi(parsing+j);
-			init_ville(&ville_table[i], tmp1, tmp2);
+			tmp2 = atoi(parsing+j);											// récup le second chiffre
+			init_ville(&ville_table[i], tmp1, tmp2);						// Initialise la ville
 		}
 		display_all_ville(ville_table, ville_table_size);
 
